fe487dfa6433c35fb466078e76b5ecebf6ada861
Sleepdealer <dreznik01@gmail.com>
2022-07-22 18:28:06 -0400

    han init

---
+++ keyboards/han60v2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBE, 0x76, 0xAD, 0x91, 0x9E, 0xA0, 0xA3, 0x2B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
