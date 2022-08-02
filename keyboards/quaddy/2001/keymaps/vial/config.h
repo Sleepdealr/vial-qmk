7ba912a848c38c789a65907b706901d38366bcdf
Sleepdealer <dreznik01@gmail.com>
2022-08-02 02:29:16 -0400

    add

---
+++ keyboards/quaddy/2001/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0xFE, 0x3C, 0x0A, 0x85, 0x3B, 0x6C, 0x22}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 6}
+#define VIAL_UNLOCK_COMBO_COLS {0, 7}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+
