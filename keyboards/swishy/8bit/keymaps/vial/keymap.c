7944e0c57d2a126949424aecb8f1310c781fc0dd
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2022-08-05 03:55:49 -0400

    fix some stuff

--- keyboards/swishy/8bit/keymaps/vial/keymap.c
+++ keyboards/swishy/8bit/keymaps/vial/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 #ifdef AUDIO_ENABLE
-    #define AUDIO_INIT_DELAY 1000
-    #define ZELDA_SONG \
+
+    #define ZELDA_SONG  \
     Q__NOTE(_G5),     \
     Q__NOTE(_A5),    \
     Q__NOTE(_B5),     \
@@ -56,29 +56,33 @@
     Q__NOTE(_AS6),     \
     Q__NOTE(_B6),    \
     H__NOTE(_C7)
+    
+    // #define STARTUP_SONG SONG(ZELDA_SONG)
 
-    //#define STARTUP_SONG SONG(ZELDA_SONG)
-    
     float my_song[][2] = SONG(ZELDA_SONG);
-    
-    enum custom_keycodes {
-        CUSTOMK = SAFE_RANGE
+
+    enum my_keycodes {
+        AUDIO1 = SAFE_RANGE
     };
 
     bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-        switch(keycode) {
-            case CUSTOMK:
-                if (record->event.pressed) {
-                    PLAY_SONG(my_song);
-                }
-                return false;
-        }
-        return true;
-    };
+    switch (keycode) {
+        case AUDIO1:
+            if (record->event.pressed) {
+                PLAY_SONG(my_song);
+            }
+            return false;
+        default:
+            return true; 
+    }
+}
+
     void keyboard_post_init_user(void) {
         PLAY_SONG(my_song);
     }
+
 #endif
+
 
 
 
@@ -97,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
     [0] = LAYOUT_60_all(
-        CUSTOMK,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+        AUDIO1,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
@@ -118,4 +122,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
-
