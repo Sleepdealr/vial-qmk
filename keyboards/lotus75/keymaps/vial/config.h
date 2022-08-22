ea70fb5269e84aed95f4ee820887aa864214dd8d
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2022-08-22 12:32:21 -0400

    lotus

---
+++ keyboards/lotus75/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0xBA, 0x46, 0xF9, 0x15, 0x54, 0x0D, 0x1B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {1, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
