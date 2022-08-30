2ab1fa861af20d5fe17bfca4104c1e310a2551e5
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2022-08-30 14:44:05 -0400

    liberi

---
+++ keyboards/swagking/liberi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB6, 0x54, 0x8D, 0x33, 0xE8, 0xEF, 0xB0, 0x77}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {2, 15}
