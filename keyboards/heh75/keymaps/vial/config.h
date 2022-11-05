86c805428fcc45eee2ba67c1dd59aa5fbf5b8b13
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2022-11-05 15:56:37 -0400

    heh + swag board

---
+++ keyboards/heh75/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC2, 0xA2, 0x55, 0xCD, 0x12, 0xB1, 0x52, 0xAC}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
---
+++ keyboards/swagking/something_frl/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0D, 0xDB, 0x46, 0x71, 0x8E, 0x67, 0x1E, 0x75}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {2, 3}
