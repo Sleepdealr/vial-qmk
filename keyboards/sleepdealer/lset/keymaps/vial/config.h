1c97d6e0f78b396e0995e0a7af3c04617cad0b50
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2023-01-31 16:24:22 -0500

    init (again)

---
+++ keyboards/sleepdealer/lset/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1A, 0x18, 0x1C, 0x46, 0xF1, 0x33, 0xD5, 0x2C}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 6}
+#define VIAL_UNLOCK_COMBO_COLS {1, 1}
