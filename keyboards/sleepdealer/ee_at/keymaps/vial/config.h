1093e5e43e1fb8c36d8a2f1b6a7d059b8276ca9e
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2023-02-01 08:32:40 -0500

    fix

---
+++ keyboards/sleepdealer/ee_at/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE8, 0x2B, 0x5C, 0x56, 0xB8, 0xB9, 0x3D, 0x3A}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {1, 1}
